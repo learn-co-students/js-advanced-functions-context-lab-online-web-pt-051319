1f13d61e33c67e9e647151fc3c8d9d14c0e826f9
Jason McGloin <jason.mcgloin@gmail.com>
2019-11-28 23:41:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
-/* Your Code Here */
+const createEmployeeRecord = (info) => {
+	return {
+		'firstName': info[0],
+		'familyName': info[1],
+		'title': info[2],
+		'payPerHour': info [3],
+		'timeInEvents': [],
+		'timeOutEvents': []
+	}
+}
+
+const createEmployeeRecords = (infos) => {
+	return infos.map(info => createEmployeeRecord(info))
+}
+
+const createTimeEvent = (event, type) => ({
+			'date': event[0],
+			'hour': parseInt(event[1]),
+			'type': type
+})
+
+ const createTimeInEvent = function(eventInfo) {
+	this.timeInEvents.push(createTimeEvent(eventInfo.split(' '), 'TimeIn'))
+	return this
+}
+
+ const createTimeOutEvent = function(eventInfo) {
+	this.timeOutEvents.push(createTimeEvent(eventInfo.split(' '), 'TimeOut'))
+	return this
+}
+
+ const hoursWorkedOnDate = function(date) {
+	let inEvent = this.timeInEvents.find(event => event.date == date)
+	let outEvent = this.timeOutEvents.find(event => event.date == date)
+	return inEvent && outEvent ? (outEvent.hour - inEvent.hour)/100 : 0
+}
+
+ const wagesEarnedOnDate = function(date) {
+	return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+ const findEmployeeByFirstName = function(collection, firstNameString){
+	return collection.find(record => record.firstName == firstNameString)
+}
+
+ const calculatePayroll = function(data) {
+	return data.reduce((payroll, employee) => allWagesFor.call(employee) + payroll, 0)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
